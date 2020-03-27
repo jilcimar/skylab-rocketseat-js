28dd29afe5bf4237d611dde44c0aef77ccfd65e0
jilcimar <jilcimarfernandes@rits.com.br>
2020-03-27 08:49:38 -0300

    mongoose e docker

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,13 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser:true, useUnifiedTopology:true});
 
+//Primeira Rota
 app.get('/', (req, res)=> {
     res.send("Hello NodeJS");
 });
