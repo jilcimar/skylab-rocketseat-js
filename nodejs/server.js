256c70624cffbcb1fe00bcce341ef2264a690dc5
jilcimar <jilcimarfernandes@rits.com.br>
2020-03-29 14:50:50 -0300

    node js criando o model

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,14 +1,28 @@
-const express = require ('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
-//Iniciando o App
+///Iniciando o App
 const app = express();
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser:true, useUnifiedTopology:true});
 
+requireDir("./src/Models");
+
+// require('./src/Models/Product');
+
+const Product = mongoose.model('Product');
+
 //Primeira Rota
 app.get('/', (req, res)=> {
+
+    Product.create({
+        title: 'React Native',
+        description: 'Build app native',
+        url: 'hhtps://www.google.com'
+    });
+
     res.send("Hello NodeJS");
 });
 
